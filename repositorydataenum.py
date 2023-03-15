25bf329b6112fc1a0846af4781455053b1c7e6df
Mattias Spangberg <spma20pm@student.ju.se>
2023-03-15 12:53:17 +0100

    added some classes

---
+++ repositorydataenum.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+#
+# This Enum contains all data that a GitHub Repository can contain. 
+# (The enum is used as input in get_repo_data function from GHRepository class)
+#
+class RepositoryData(Enum):
+    NAME = 0
+    HAS_GHA = 1
