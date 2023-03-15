7ba3274990e8990b45a2e06248944874ecc4e412
AlbinWiklund <albin.wiklund11@gmail.com>
2023-03-15 15:14:39 +0100

    save interval fun

--- testfunctions.py
+++ testfunctions.py
@@ -1,5 +1,6 @@
 from ghrepository import GHRepository
 from repositorydataenum import RepositoryData
+from view import View
 
 
 def test_create_and_get_data_from_repository():
@@ -9,5 +10,8 @@ def test_create_and_get_data_from_repository():
     repo.add_data_by_name(RepositoryData.NAME.name, "first repo")
 
     print(repo.get_repository_data_by_name(RepositoryData.NAME.name))
+    
+def test_user_settings_from_view():
+    print(View.set_save_interval())
 
 test_create_and_get_data_from_repository()
---
+++ view.py
@@ -0,0 +1,7 @@
+class View:
+    def set_save_interval() -> int:
+        answer = ""
+        print("How many repositories do you want each save?")
+        answer = int(input())
+        return answer
+    
